83cb79c7b731844d3a7e6349a3ae1b51193366f6
hillarioh <okerioh@gmail.com>
2020-05-02 20:05:37 +0300

    added checks

--- lib/board.rb
+++ lib/board.rb
@@ -27,9 +27,76 @@ class Board
     end
 
 
-    def results_check
+    def results_check(position,p_id)
+
+        # Vertical check
+         if position < 4
+        
+
+        # Horizontal check
+
+        #diagonal check
         
     end
+
+    def vertical_check(position,p_id)
+
+        my_array = []
+       
+        case position
+        when 1..3
+            my_array <<  my_board[position]
+            my_array <<  my_board[position+3]
+            my_array <<  my_board[position+6]
+        when 4..6
+            my_array <<  my_board[position-3]
+            my_array <<  my_board[position]
+            my_array <<  my_board[position+3]
+        else
+            my_array <<  my_board[position-6]
+            my_array <<  my_board[position-3]
+            my_array <<  my_board[position]
+        end
+
+        return my_array.all?{|i| i==p_id}
+
+    end
+
+    def horizontal_check(position,p_id)
+
+        my_array = []
+
+        if position % 3 == 0
+            my_array << my_board[position-2]
+            my_array << my_board[position -1]
+            my_array << my_board[position]
+        elsif position % 3 == 1
+            my_array << my_board[position]
+            my_array << my_board[position+1]
+            my_array << my_board[position+2]
+        else
+            my_array << my_board[position-1]
+            my_array << my_board[position]
+            my_array << my_board[position+1]
+        end
+
+         return my_array.all?{|i| i==p_id}
+
+    end
+
+    def diag_check(position,p_id)
+
+        case position
+        when 1,5,9
+            my_array = [my_board[1],my_board[5],my_board[9]]
+        when 3,5,7
+            my_array = [my_board[1],my_board[5],my_board[9]]
+        end
+
+        return my_array.all?{|i| i==p_id}
+
+    end
+
 
 
     
