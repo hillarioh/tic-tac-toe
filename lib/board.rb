63505453f27940104ee20778ceb5b548a5762877
hillarioh <okerioh@gmail.com>
2020-05-05 07:15:35 +0300

    correct linter errors

--- lib/board.rb
+++ lib/board.rb
@@ -1,118 +1,110 @@
 class Board
+  attr_reader :my_board, :results
 
-    attr_reader :my_board, :results
+  def initialize()
+    @my_board = {
+      1 => ' ',
+      2 => ' ',
+      3 => ' ',
+      4 => ' ',
+      5 => ' ',
+      6 => ' ',
+      7 => ' ',
+      8 => ' ',
+      9 => ' '
+    }
+    @results = false
+  end
 
+  def input_position(value, player_id)
+    # Checks if position is filled
+    if my_board[value] == ' '
+      # replaces id with X and O
+      if player_id.zero?
+        my_board[value] = 'X'
+        @results = results_check(value, 'X')
+      else
+        my_board[value] = 'O'
+        @results = results_check(value, 'O')
+      end
 
-    def initialize()
-        @my_board={
-            1=>" ",
-            2=>" ",
-            3=>" ",
-            4=>" ",
-            5=>" ",
-            6=>" ",
-            7=>" ",
-            8=>" ",
-            9=>" ",
-        }
-        @results=  false
+    else
+      Main.statuses('position filled, choose another')
+      new_val = Main.enter_position('Try again')
+      input_position(new_val, player_id)
+    end
+  end
+
+  def results_check(position, p_id)
+    vertical = vertical_check(position, p_id)
+    horiz = horizontal_check(position, p_id)
+    diag = diag_check(position, p_id)
+
+    # check if any check for win is true
+    [vertical, horiz, diag].any?
+  end
+
+  # Check for vertical win
+  def vertical_check(position, p_id)
+    my_array = []
+
+    case position
+    when 1..3
+      my_array << my_board[position]
+      my_array << my_board[position + 3]
+      my_array << my_board[position + 6]
+    when 4..6
+      my_array << my_board[position - 3]
+      my_array << my_board[position]
+      my_array << my_board[position + 3]
+    else
+      my_array << my_board[position - 6]
+      my_array << my_board[position - 3]
+      my_array << my_board[position]
     end
 
-   
-def input_position(value,player_id)
-        # Checks if position is filled
-       if self.my_board[value] ==" "
-        # replaces id with X and O
-        if player_id == 0
-            self.my_board[value]="X"
-            @results = results_check(value,"X")
-        else
-            self.my_board[value]="O"
-            @results = results_check(value,"O")
-        end
+    my_array.all? { |i| i == p_id }
+  end
 
-       else
-        Main.statuses("position filled, choose another")
-        new_val = Main.enter_position("Try again")
-        input_position(new_val,player_id)
-       end
+  # Check for vertical win
+  def horizontal_check(position, p_id)
+    my_array = []
+
+    if (position % 3).zero?
+      my_array << my_board[position - 2]
+      my_array << my_board[position - 1]
+      my_array << my_board[position]
+    elsif position % 3 == 1
+      my_array << my_board[position]
+      my_array << my_board[position + 1]
+      my_array << my_board[position + 2]
+    else
+      my_array << my_board[position - 1]
+      my_array << my_board[position]
+      my_array << my_board[position + 1]
     end
 
-    
-    def results_check(position,p_id)
+    my_array.all? { |i| i == p_id }
+  end
 
-        vertical = vertical_check(position,p_id)
-        horiz = horizontal_check(position,p_id)
-        diag = diag_check(position,p_id)
+  # Check for diagonal win
+  def diag_check(position, p_id)
+    case position
+    when 1, 9
+      my_array = [my_board[1], my_board[5], my_board[9]]
+    when 3, 7
+      my_array = [my_board[3], my_board[5], my_board[7]]
+    when 5
+      left_dig = [my_board[1], my_board[5], my_board[9]]
+      ryt_dig = [my_board[3], my_board[5], my_board[7]]
 
-        # check if any check for win is true
-        return [vertical,horiz,diag].any?
+      my_array = left_dig if left_dig.all? { |i| i == p_id }
 
+      my_array = ryt_dig if ryt_dig.all? { |i| i == p_id }
+    else
+      return false
     end
 
-    # Check for vertical win
-    def vertical_check(position,p_id)
-
-        my_array = []
-
-        case position
-        when 1..3
-            my_array <<  my_board[position]
-            my_array <<  my_board[position+3]
-            my_array <<  my_board[position+6]
-        when 4..6
-            my_array <<  my_board[position-3]
-            my_array <<  my_board[position]
-            my_array <<  my_board[position+3]
-        else
-            my_array <<  my_board[position-6]
-            my_array <<  my_board[position-3]
-            my_array <<  my_board[position]
-        end
-
-        return my_array.all?{|i| i==p_id}
-
-    end
-
-     # Check for vertical win
-    def horizontal_check(position,p_id)
-
-        my_array = []
-
-        if position % 3 == 0
-            my_array << my_board[position-2]
-            my_array << my_board[position -1]
-            my_array << my_board[position]
-        elsif position % 3 == 1
-            my_array << my_board[position]
-            my_array << my_board[position+1]
-            my_array << my_board[position+2]
-        else
-            my_array << my_board[position-1]
-            my_array << my_board[position]
-            my_array << my_board[position+1]
-        end
-
-         return my_array.all?{|i| i==p_id}
-
-    end
-
-     # Check for diagonal win
-    def diag_check(position,p_id)
-
-        if position % 2 == 0
-            return false
-        end
-
-        case position
-        when 1,5,9
-            my_array = [my_board[1],my_board[5],my_board[9]]
-        when 3,5,7
-            my_array = [my_board[3],my_board[5],my_board[7]]
-        end
-
-        return my_array.all?{|i| i==p_id}
-
-    end
+    my_array.all? { |i| i == p_id }
+  end
 end
-
--- lib/game.rb
+++ lib/game.rb
@@ -2,62 +2,53 @@ require_relative '../bin/main.rb'
 require_relative 'player.rb'
 require_relative 'board.rb'
 
-class Game;end
-
-if Main.welcome == false
-    return Main.statuses("You did not press 1")
+class Game
+  def initialize; end
 end
 
-player1 = Player.new(Main.enter_name("Player 1"))
-player2 = Player.new(Main.enter_name("Player 2"))
+return Main.statuses('You did not press 1') if Main.welcome == false
 
-new_game = Board.new()
+player1 = Player.new(Main.enter_name('Player 1'))
+player2 = Player.new(Main.enter_name('Player 2'))
+
+new_game = Board.new
 
 # maximum number of moves a player can make
 maximum_moves = 5
 
 loop do
+  Main.statuses('Let the game begin') if maximum_moves == 5
 
-    if maximum_moves == 5 
-        Main.statuses("Let the game begin")
-    end
+  # displays current board state
+  Main.display_board(new_game.my_board)
 
-    # displays current board state 
+  # Player One
+  position = Main.enter_position(player1.player_name, player1.player_id)
+  new_game.input_position(position, player1.player_id)
+
+  # displays current board state after player one input
+  Main.display_board(new_game.my_board)
+
+  # checks for true in results
+  if new_game.results
+    Main.statuses("#{player1.player_name} has won")
+    break
+  end
+
+  # Player two
+  position = Main.enter_position(player2.player_name, player2.player_id)
+  new_game.input_position(position, player2.player_id)
+
+  # checks for true in results
+  if new_game.results
     Main.display_board(new_game.my_board)
+    Main.statuses("#{player2.player_name} has won")
+    break
+  end
 
-    # Player One
-    position = Main.enter_position(player1.player_name,player1.player_id)
-    new_game.input_position(position,player1.player_id)
+  Main.statuses('The game is a draw') if maximum_moves == 1
 
-    # displays current board state after player one input
-    Main.display_board(new_game.my_board)
+  break if maximum_moves == 1
 
-    # checks for true in results
-    if new_game.results
-        Main.statuses("#{player1.player_name} has won")
-        break
-    end
-
-    # Player two
-    position = Main.enter_position(player2.player_name,player2.player_id)
-    new_game.input_position(position,player2.player_id)
-
-    # checks for true in results
-    if new_game.results
-        Main.display_board(new_game.my_board)
-        Main.statuses("#{player2.player_name} has won")
-        break
-    end
-
-    if maximum_moves ==1
-        Main.statuses("The game is a draw")
-    end
-
-    break if  maximum_moves==1
-    maximum_moves -=1
-    
+  maximum_moves -= 1
 end
-
-
-
-
