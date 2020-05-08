e5b69a098277a39f56a59e40c64cf8718c7d78da
calvinoea <calvinebunamu@gmail.com>
2020-05-08 14:40:17 +0100

    changes to test file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,22 +1,61 @@
-require '../lib/main.rb'
+require './bin/main.rb'
 
-describe start_game do
- describe "#new game" do
-   it "creates new players" do
-	new_game = Game.new
-        new_game.set_players
+def start_game
+  new_game = Game.new
 
-       expect(game.my_pos.to_i.positive? && game.my_pos.to_i).to eq(9)
-  end
- end
+  return statuses('You did not press 1') if welcome == false
 
-describe '#if player wins' do
- if 'display game status'
-   (expect new_game.new_board.results).to output ("#{current_player} has won")
-  end
- end
+  new_game.set_players
+
+  # maximum number of moves a player can make
+  maximum_moves = 9
 end
 
+describe './bin/main.rb' do
+  describe Game do
+    describe '#initialize' do
+      it 'assigns @board to a hash spaces " "' do
+        @new_board = Board.new
+        expect(new_board.my_board).to eq({
+      1 => ' ',
+      2 => ' ',
+      3 => ' ',
+      4 => ' ',
+      5 => ' ',
+      6 => ' ',
+      7 => ' ',
+      8 => ' ',
+      9 => ' '
+    })
+      end
+    end
 
 
+    describe '#moves' do
+      it 'returns true for a draw' do
+        expect(maximum_moves).to eq(9)
+      end
 
+    describe '#player id input' do
+      it 'returns true for a draw' do
+        expect(choice.to_i).to eq(1)
+        expect(sec_choice.to_i).to eq(1)
+      end
+
+      describe 'moves' do
+      it 'checks for valid input' do
+      expect(my_pos.to_i).to be(true)
+    end
+
+    describe 'Wins' do
+      it 'defines wins with has for combinations' do
+          current_player_id = new_game.player1.player_id
+
+          expect(vertical).to eql(vertical_check(position, p_id))
+          expect(horiz ).to eql(horizontal_check(position, p_id))
+
+      end
+    end
+
+  end
+end
